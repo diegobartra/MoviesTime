6fee607abf6cc648698d51d51ef7c58980c5dfbc
Diego Bartra <diegobartra@Diegos-MacBook-Pro.local>
2016-10-27 14:38:09 -0500

    Initial Commit

---
+++ MoviesTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoviesTime
+//
+//  Created by Diego Bartra on 10/27/16.
+//  Copyright © 2016 ArcTouch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MoviesTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoviesTime
+//
+//  Created by Diego Bartra on 10/27/16.
+//  Copyright © 2016 ArcTouch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
